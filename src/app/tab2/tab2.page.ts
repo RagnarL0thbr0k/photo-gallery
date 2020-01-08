67de961be94537ab1ac4b2bb7af052d78d9df96b
Giacomo Zidarich <giacomo.zidarich@teorema.net>
2020-01-08 18:22:34 +0100

    Last version funzionante

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PhotoService } from '../services/photo.service';
 
 //esempio di interpolazione
 @Component({
@@ -17,21 +18,9 @@ export class Tab2Page {
 
   //fotocoamera
   currentImage: any;
-  constructor(private camera: Camera) { }
+  constructor(public photoService: PhotoService) {  }
 
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
-
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-      // Handle error
-      console.log("Camera issue:" + err);
-    });
+  ngOnInit() {
+    this.photoService.loadSaved();
   }
 }
